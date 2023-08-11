7e870745cdf055404c0deb0a3370efbc2baf69c2
mrloobo <mahajandhiraj99@gmail.com>
2023-08-11 17:30:26 +0530

    Changed class names of finalfooterfile of labding page

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import LandingPage from "./Components/LandingPage"
+import LandingPage from "./Components/LandingPage";
 
 function App() {
   return <>
