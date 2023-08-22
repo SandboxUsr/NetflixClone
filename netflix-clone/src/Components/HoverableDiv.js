95d987e1f7d058443e6e85d592c3473c2aa21298
Muskan-Gupta10 <muskangupta1018@gmail.com>
2023-08-22 22:03:21 +0530

    HoverableDiv.js added

---
+++ netflix-clone/src/Components/HoverableDiv.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import '../Styles/hoverableDiv.css'; // Import your CSS file for styling
+import OnHoverCard from './OnHoverCard';
+
+
+const HoveredComponent = () => {
+  return <div className="hovered-card"><OnHoverCard/></div>;
+};
+
+const ContainerComponent = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div
+      className="container"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="hoverable-card">
+        <div>Hover over me</div>
+        {isHovered && <HoveredComponent />}
+      </div>
+    </div>
+  );
+};
+
+export default ContainerComponent;
