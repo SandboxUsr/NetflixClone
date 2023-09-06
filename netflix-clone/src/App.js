729acb8d904e29d0968b1cdbf3a57ff61ac09468
Muskan-Gupta10 <muskangupta1018@gmail.com>
2023-09-06 23:58:57 +0530

    some changes

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,19 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import HomePage from "./Components/HomePage";
-import TvShows from "./Components/TvShows";
-import Latest from "./Components/Latest";
-import LandingPage from "./Components/LandingPage"
-import MyList from "./Components/MyList"
-import LoginPage from "./Components/LoginPage"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import HomePage from "./Components/HomePage";
+// import TvShows from "./Components/TvShows";
+// import Latest from "./Components/Latest";
+// import LandingPage from "./Components/LandingPage"
+// import MyList from "./Components/MyList"
+// import LoginPage from "./Components/LoginPage"
+import OnHoverCard from "./Components/OnHoverCard"
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          {/* <Route path="/signin" element={<LoginPage />} /> */}
+          
         </Routes>
         <Routes>
           <Route path="/home" element={<HomePage />} />
@@ -22,8 +23,10 @@ function App() {
           <Route path="/latest" element={<Latest />} />
           <Route path="/mylist" element={<MyList />} />
           <Route path="/loginpage" element={<LoginPage/>} />
+          <Route path="/onhovercard" element={<OnHoverCard/>} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <OnHoverCard/>
     </>
   );
 }
--- netflix-clone/src/Components/OnHoverCard.js
+++ netflix-clone/src/Components/OnHoverCard.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function OnHoverCard() {
   return (
-    <div>OnHoverCard</div>
+    <div id='card-container'>
+        <div id="card-img"></div>
+        <div id="card-buttons"></div>
+        <div id="card-details"></div>
+    </div>
   )
 }
 
