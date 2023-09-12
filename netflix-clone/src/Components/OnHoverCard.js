395ad99795ca4b1fc77f75898f99be9d1e5f5de1
Muskan-Gupta10 <muskangupta1018@gmail.com>
2023-09-12 21:41:54 +0530

    onhovercard.js updated

--- netflix-clone/src/Components/OnHoverCard.js
+++ netflix-clone/src/Components/OnHoverCard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import demo from '../Assets/demo.jpg'
+//import { BsPlayCircleFill } from 'react-icons/ba';
+//import { MdOutlineDoneOutline } from 'react-icons/md';
+//import { IoMdThumbsUp } from 'react-icons/io';
+//import { FaCircleChevronDown } from 'react-icons/fa';
+
 
 function OnHoverCard() {
   return (
-    <div id='card-container'>
-        <div id="card-img"></div>
+    <div className='h-96 w-1/3 bg-black' >
+        <div className={`bg-[url(${demo})] bg-[center_top_1rem] h-[190px] w-[440px] `}>
+          
+        </div>
         <div id="card-buttons"></div>
         <div id="card-details"></div>
     </div>
